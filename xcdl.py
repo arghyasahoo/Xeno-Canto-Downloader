1a903e44481016920c3de3e8237c8653856abb8f
Arghya Sahoo <63043486+arghyasahoo@users.noreply.github.com>
2022-07-04 00:15:25 +0530

    Refactored code

--- xcdl.py
+++ xcdl.py
@@ -1,37 +1,39 @@
-import json, os, requests as r
 import pandas as pd
 from urllib.parse import quote
+import json, os, requests as r, urllib.request as req
 
 
 # os.system("mkdir sounds")
 species = ['Acridotheres fuscus', 'Acrocephalus bistrigiceps', 'Aethopyga gouldiae', 'Alcippe cinerea', 'Arachnothera magna', 'Arborophila torqueola', 'Argya longirostris', 'Centropus andamanensis', 'Chelidorhynx hypoxanthus', 'Chloropsis cochinchinensis', 'Chloropsis hardwickii', 'Chloropsis jerdoni', 'Chrysococcyx maculatus', 'Cuculus micropterus', 'Cyornis concretus', 'Cyornis magnirostris', 'Cyornis poliogenys', 'Cyornis unicolor', 'Cypsiurus balasiensis', 'Dicaeum chrysorrheum', 'Dicaeum cruentatum', 'Dicaeum minullum', 'Dicrurus andamanensis', 'Eurystomus orientalis', 'Glaucidium cuculoides', 'Halcyon coromanda', 'Iole cacharensis', 'Ixobrychus cinnamomeus', 'Lanius tephronotus', 'Liocichla phoenicea', 'Locustella davidi', 'Loriculus vernalis', 'Macronus gularis', 'Motacilla citreola', 'Mystery mystery', 'Paradoxornis guttaticollis', 'Pellorneum ruficeps', 'Phylloscopus forresti', 'Phylloscopus inornatus', 'Phylloscopus subaffinis', 'Pluvialis squatarola', 'Pnoepyga pusilla', 'Polyplectron bicalcaratum', 'Pomatorhinus ruficollis', 'Psilopogon lineatus', 'Psittacula eupatria', 'Rimator malacoptilus', 'Riparia chinensis', 'Spelaeornis oatesi', 'Sphenocichla humei', 'Stachyridopsis ambigua', 'Sturnia malabarica', 'Todiramphus chloris']
 
-sub_species = species[27:40]
+sub_species = species[27:30]
+urls= ""
 
 for s in sub_species:
-    print(s)
     url = f"https://www.xeno-canto.org/api/2/recordings?query={quote(s)}%20cnt:\"%3DIndia\""
-    print(url)
-
-    res = r.get(url);
-    with open("noca-query.json", 'w') as dataset:
-        dataset.write(res.content.decode())
+    req.urlretrieve(url, "xc-query.json")
 
     # Get the json entries from your downloaded json
-    jsonFile = open('noca-query.json', 'r')
+    jsonFile = open('xc-query.json', 'r')
     values = json.load(jsonFile)
     jsonFile.close()
 
     # Create a pandas dataframe of records & convert to .csv file
     record_df = pd.DataFrame(values['recordings'])
-    record_df.to_csv('xc-noca.csv', index=False)
+    record_df.to_csv('xc-res.csv', index=False)
 
     # Make wget input file
     url_list = []
     for file in record_df['file'].tolist():
         url_list.append(file)
-    with open('xc-noca-urls.txt', 'w+') as f:
+    with open('xc-res-urls.txt', 'a+') as f:
         for item in url_list:
-            f.write("{}\n".format(item))
+            f.write(item)
+            f.write("\n")
 
-    os.system(f"wget -P sounds/\"{s}\" --trust-server-names -i xc-noca-urls.txt")
+
+with open("xc-res-urls.txt", 'r') as urls_file:
+    urls = urls_file.readlines();
+
+for url in urls:
+    req.urlretrieve(url, f"XC{url.split('/')[3]}.mp3")
